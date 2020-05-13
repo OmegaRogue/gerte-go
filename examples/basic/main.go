e05cad327b88c99aa3a7af712f00ae95c59e9997
OmegaRogue <thatomegarogue@gmail.com>
2020-05-13 10:52:07 +0200

    Add example
    Signed-off-by: OmegaRogue <thatomegarogue@gmail.com>

---
+++ examples/basic/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/OmegaRogue/gerte-go"
+)
+
+var Address string
+var Key string
+var ServerAddress string
+
+func init() {
+	Address = os.Getenv("ADDR")
+	Key = os.Getenv("KEY")
+	ServerAddress = os.Getenv("SERVER_ADDR")
+}
+
+func main() {
+
+	api := gerte.NewApi(gerte.Version{
+		Major: 1,
+		Minor: 1,
+		Patch: 0,
+	})
+	con, err := net.Dial("tcp", "localhost:43780")
+	if err != nil {
+		log.Fatalf("error on tcp dial: %+v", err)
+	}
+	err = api.Startup(con)
+	if err != nil {
+		log.Fatalf("error on startup: %+v", err)
+	}
+	addr := gerte.GertAddress{
+		Upper: 0123,
+		Lower: 0456,
+	}
+	register, err := api.Register(addr, "11111111111111111111")
+	if err != nil {
+		log.Fatalf("error on register: %+v", err)
+	}
+	log.Printf("registered: %v", register)
+
+	targ := gerte.GERTc{
+		GERTe: addr,
+		GERTi: gerte.GertAddress{
+			Upper: 1,
+			Lower: 1,
+		},
+	}
+	pkt := gerte.Packet{
+		Source: targ,
+		Target: targ,
+		Data:   []byte("test"),
+	}
+	transmit, err := api.Transmit(pkt)
+	if err != nil {
+		log.Fatalf("error on transmit: %+v", err)
+	}
+	log.Printf("transmitted: %v", transmit)
+
+	err = api.Shutdown()
+	if err != nil {
+		log.Fatalf("error on shutdown: %+v", err)
+	}
+}
+
+func versionToBytes(ver gerte.Version) []byte {
+	return []byte{ver.Major, ver.Minor, ver.Patch}
+}
+
+func toBytes(addr gerte.GertAddress) []byte {
+	var b strings.Builder
+	b.WriteByte(byte(addr.Upper >> 4))
+	b.WriteByte(byte(((addr.Upper & 0x0F) << 4) | (addr.Lower >> 8)))
+	b.WriteByte(byte(addr.Lower & 0xFF))
+	return []byte(b.String())
+}
